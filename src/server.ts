8eede1d9b7ec2715f8726ebe4b5da45ac13a15fb
Luis Cov Jr <shusten@hotmail.com>
2020-10-14 03:00:45 -0300

    :construction: adiconando dependencias

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) => {
     return response.json({ message: 'Hello World' });
 })
