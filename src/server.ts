6c6dd23d05c5e4e51ee9adf986ccdb7a33826229
Luis Cov Jr <shusten@hotmail.com>
2020-10-14 02:42:50 -0300

    :white_check_mark: Hello World Json

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { response } from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-    return response.send('Hello World');
+    return response.json({ message: 'Hello World' });
 })
 
 app.listen(3333);
