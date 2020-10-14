254ba8c0ac56b6c060949fceb58221f65a853cde
Luis Cov Jr <shusten@hotmail.com>
2020-10-14 02:40:52 -0300

    :sparkles: feat: Hello World

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+
+app.get('/users', (request, response) => {
+    return response.send('Hello World');
+})
 
 app.listen(3333);
 
